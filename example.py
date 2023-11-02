f8bc450e0bd556eb60e240ca681278033af25d14
goshva <sha.egor@gmail.com>
2023-11-02 15:14:51 +0300

    gogogo

--- example.py
+++ example.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
+from quesstionnare import Questionnaire
 from typing import Final
 from telegrambot import TeleBot, filters
 import asyncio
 
-BOT_USERNAME: Final = 'your_bot_name'
-token = 'your_token_key'
-
-with open("security_token.txt", "r") as f:
+BOT_USERNAME: Final = 'Lang_Lear_Sort_Tele_Bot'
+token = ''
+with open(".keys", "r") as f:
     token = f.read()
 
 app = TeleBot(token)
@@ -27,5 +27,31 @@ async def getchatid(update, context):
 
 #app.send_to_user("hello?", "your_chat_id")
 #app.send_to_users("hello?", ["your_chat_id", "your_chat_id"])
+
+
+@app.handle_command("go")
+async def getchatid(update, context):
+    # Create a questionnaire instance
+    questionnaire = Questionnaire()
+
+    # Add questions to the questionnaire
+    await update.message.reply_text(f"What is your favorite color?")
+    print(update.message)
+    questionnaire.add_question("How old are you?")
+    questionnaire.add_question("Do you like pizza?")
+
+    # Add branching logic to the questionnaire
+    questionnaire.add_branch("What is your favorite color?", "blue", "You like blue.")
+    questionnaire.add_branch("How old are you?", "18", "You are 18 years old.")
+    questionnaire.add_branch("Do you like pizza?", "yes", "You like pizza.")
+
+    # Run the questionnaire
+    questionnaire.run_questionnaire()
+
+
+    await update.message.reply_text(f"your chatid is")
+
+
+
 
 app.start()
---
+++ tests/rotator.py
@@ -0,0 +1,28 @@
+import os
+import cv2
+
+def rotate_images_in_folder(folder_path):
+    img_extensions = ['.jpg', '.jpeg', '.png', '.gif']
+    
+    for file in os.listdir(folder_path):
+        print(file)
+        ext = os.path.splitext(file)[1].lower()
+        if ext in img_extensions:
+            img_path = os.path.join(folder_path, file)
+            print(img_path)
+            img = cv2.imread(img_path)
+            
+            # Determine if the image is vertical
+            height, width = img.shape[:2]
+            if height > width:
+                # Rotate the image by 90 degrees counter-clockwise
+                rotated_img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+                
+                # Save the rotated image back to the original path
+                cv2.imwrite(img_path, rotated_img)
+
+# Specify the path to the folder containing the images
+folder_path = './'
+
+# Call the function to rotate all vertical images in the specified folder
+rotate_images_in_folder(folder_path)
